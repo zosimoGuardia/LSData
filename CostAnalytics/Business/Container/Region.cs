b1bffd7ef878fcfefce2a78c3ada810f5b8422bc
Dominic_Bett <Dominic_Bett@Dell.com>
2017-01-16 11:49:27 -0600

    - Reordering Folders

---
+++ CostAnalytics/Business/Container/Region.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Container
+{
+    class Region
+    {
+        public int ID { get; set; }
+        public string FacilityName { get; set; }
+        public string RegionName { get; set; }
+        public string Country { get; set; }
+        public string CCN { get; set; }
+    }
+}
