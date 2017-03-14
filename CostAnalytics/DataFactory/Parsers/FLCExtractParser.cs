214a265ad08b6c5f216c3910647ac7eb5f21d3d6
Dominic_Bett <Dominic_Bett@Dell.com>
2017-03-14 15:58:37 -0500

    updated namespace, created parser project

---
+++ CostAnalytics/DataFactory/Parsers/FLCExtractParser.cs
@@ -0,0 +1,51 @@
+﻿using LumenWorks.Framework.IO.Csv;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dell.CostAnalytics.DataFactory.Parsers
+{
+    /// <summary>
+    /// Class to extracts data from FLC_EXTRACT_EUC_PC_* File
+    /// </summary>
+    public class FLCExtractParser
+    {
+        #region Constructors
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public FLCExtractParser()
+        {
+
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Method to Parse the CSV from the file
+        /// </summary>
+        /// <param name="fileName"></param>
+        public void ParseData(string fileName)
+        {
+            using (CachedCsvReader csv = new CachedCsvReader(new StreamReader(fileName), true))
+            {
+                int fieldCount = csv.FieldCount;
+
+                string[] headers = csv.GetFieldHeaders();
+                while (csv.ReadNextRecord())
+                {
+                    for(int i = 0; i < fieldCount; i++)
+                    {
+                        Console.WriteLine(string.Format("{0} = {1};",
+                              headers[i], csv[i]));
+                        Console.Read();
+                    }//end for
+                }//end while
+            }//end using
+        }//end method
+        #endregion
+    }//end class
+}//end namespace
---
+++ CostAnalytics/DataFactoryTest/FLCExtractParserTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Factory = Dell.CostAnalytics.DataFactory;
+
+namespace DataFactoryTest
+{
+    [TestClass]
+    public class FLCExtractParserTest
+    {
+        [TestMethod]
+        public void TestParseData()
+        {
+            System.Diagnostics.Debug.Write("Unit test TestParseData has started...\n");
+            string fileName = "C:\\Users\\Dominic_Bett\\Desktop\\Desktop Items\\PROJECTS\\Cost Analysis\\FLC_EXTRACT_EUC_PC_20161207051244.csv";
+            Factory.Parsers.FLCExtractParser parser = new Factory.Parsers.FLCExtractParser();
+            parser.ParseData(fileName);
+
+            System.Diagnostics.Debug.Write("Unit test TestParseData has ended...\n");
+        }
+    }
+}
