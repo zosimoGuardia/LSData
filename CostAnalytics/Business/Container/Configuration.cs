92111b1b2d2c7c5b5040f0d890917e6f2e4f4994
Dominic_Bett <Dominic_Bett@Dell.com>
2017-03-27 11:50:41 -0500

    - Enabled functionality to filter data

--- CostAnalytics/Business/Container/Configuration.cs
+++ CostAnalytics/Business/Container/Configuration.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Dell.CostAnalytics.Business.Container
 {
-    class Configuration
+    public class Configuration
     {
         public int ID { get; set; }
         public string Name { get; set; }
--- CostAnalytics/Business/Container/Product.cs
+++ CostAnalytics/Business/Container/Product.cs
@@ -6,12 +6,71 @@ using System.Threading.Tasks;
 
 namespace Dell.CostAnalytics.Business.Container
 {
-    class Product
+    public class Product
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string LOB { get; set; }
-        public int Model { get; set; }
-        public string Variant { get; set; }
-    }
-}
+        #region Constructors
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public Product()
+        {
+        } //end constructor
+
+        /// <summary>
+        /// Costructor - Initializes all properties
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <param name="Name"></param>
+        /// <param name="LOB"></param>
+        /// <param name="Model"></param>
+        /// <param name="Variant"></param>
+        public Product(int ID, string Name, string LOB, int Model, string Variant)
+        {
+            m_ID = ID;
+            m_Name = Name;
+            m_LOB = LOB;
+            m_Model = Model;
+            m_Variant = Variant;
+        }//end constructor
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Property for Product ID
+        /// </summary>
+        public int ID {
+            get { return m_ID; }
+            set { m_ID = value; }
+        }//end property
+
+        /// <summary>
+        /// Property for Product Name
+        /// </summary>
+        public string Name {
+            get { return m_Name; }
+            set { m_Name = value; }
+        }//end property
+
+        public string LOB {
+            get { return m_LOB; }
+            set { m_LOB = value; }
+        }
+        public int Model {
+            get { return m_Model; }
+            set { m_Model = value; }
+        }
+        public string Variant {
+            get { return m_Variant; }
+            set { m_Variant = value; }
+        }
+        #endregion
+
+        #region Members
+        private int m_ID;
+        private string m_Name;
+        private string m_LOB;
+        private int m_Model;
+        private string m_Variant;
+        #endregion
+    }//end class
+}//end namespace
