be54ea546f9c55ea042020360424bb6bc6487ee3
Zosimo Guardia Rullan <zosimo@bu.edu>
2017-06-12 14:51:39 -0500

    Added WarrantyInfoParser to go through warranty data

---
+++ CostAnalytics/DataFactory/Parsers/WarrantyInfoParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dell.CostAnalytics.DataFactory.Parsers
+{
+    class WarrantyInfoParser : IDisposable
+    {
+        #region Constructors
+        /// <summary> This constructor takes the warranty file path and sets it as an attribute. </summary>
+        /// <param name="warrantyFileName"> File that contains the warranty information. </param>
+        public WarrantyInfoParser(string warrantyFilePath)
+        {
+            this.m_warrantyInfoFilePath = warrantyFilePath;
+        } //End constructor (string)
+        #endregion
+
+        #region Methods
+        /// <summary> Disposes object </summary>
+        public void Dispose()
+        {
+            this.Dispose();
+        }
+        #endregion
+
+        #region Subclasses
+        public class WarrantyInfo
+        {
+            public string Name { get; set; }
+            public string P
+        }
+        #endregion
+
+        #region Properties
+        private string m_warrantyInfoFilePath;
+        #endregion
+    }
+}
